be1e78e48c43d57f2f78c664472c3967e2d6eee2
Mugta1 <mayankgupta_23bt043@dtu.ac.in>
2024-06-12 13:22:53 +0530

    Code works, solution is being produced

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,7 +1,8 @@
 ##sudoku solver
+##the code is working as of now 
 
 import numpy as np
-
+#Enter your sudoku in the form of 2d matrix 9x9
 sudoku= np.array([[5,3,0,0,7,0,0,0,0],[6,0,0,1,9,5,0,0,0],[0,9,8,0,0,0,0,6,0],[8,0,0,0,6,0,0,0,3],[4,0,0,8,0,3,0,0,1],[7,0,0,0,2,0,0,0,6],[0,6,0,0,0,0,2,8,0],[0,0,0,4,1,9,0,0,5],[0,0,0,0,8,0,0,7,9]])
 
 #checks if the arrays given for sudoku are of correct size.
